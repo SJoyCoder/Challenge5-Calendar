b59814cb2e08c045563c75f11d7acd70fe7d9d17
Savannah McGinnis <sjmhorsegirl@verizon.net>
2022-09-08 23:11:03 -0500

    working on keeping time updated

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,7 @@
 let time = moment().format("LT");
+function displayTime() {
+    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(rightNow);
+    setInterval(displayTime, 1000);
 
 console.log(time);
