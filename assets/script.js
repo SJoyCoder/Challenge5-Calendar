fd621a1750e0e867b6256b0f72fecd6cfc31c318
Savannah McGinnis <sjmhorsegirl@verizon.net>
2022-09-09 15:52:45 -0500

    added event listener and local storage

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,25 @@
-let time = moment().format("LT");
+let time = moment().format("H");
+let saveBtns = document.querySelectorAll(".saveBtn");
+let appointment= document.querySelectorAll(".time-block")
 function displayTime() {
-    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    var rightNow = moment().format('H');
     setInterval(1000);
+    
 
-console.log(time);
+};
+console.log(time)
+
+for (let i = 0; i < saveBtns.length; i++) {
+
+    console.log(appointment[i].getAttribute("data-time"));
+
+    appointment[i].value = localStorage.getItem("appt"+i)
+
+    saveBtns[i].addEventListener("click", (tacocat) =>{
+        console.log(tacocat.target);
+        console.log(appointment[i].value);
+        localStorage.setItem("appt"+i, appointment[i].value)
+    })
+
+
+}
