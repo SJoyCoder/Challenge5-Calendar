314018e63f5d53fdeffc193eedcf37d29ec6b3de
Savannah McGinnis <sjmhorsegirl@verizon.net>
2022-09-09 23:10:10 -0500

    it works

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 let time = moment().format("H");
 let saveBtns = document.querySelectorAll(".saveBtn");
 let appointment= document.querySelectorAll(".time-block")
+let past = document.querySelector(".past")
+
 function displayTime() {
     var rightNow = moment().format('H');
     setInterval(1000);
@@ -12,12 +14,16 @@ console.log(time)
 for (let i = 0; i < saveBtns.length; i++) {
 
     if (appointment[i].getAttribute("data-time") === time){
-        appointment[i].style.present
+        $(appointment[i]).addClass("present");
     }else if(appointment[i].getAttribute("data-time") < time){
-        appointment[i].future
+        $(appointment[i]).addClass("past");
     }else if(appointment[i].getAttribute("data-time") > time){
-        appointment[i].past
+        $(appointment[i]).addClass("future");
     };
+
+    $("button").click(function(){
+        $(appointment[i]).addClass("future");
+      });
 
     appointment[i].value = localStorage.getItem("appt"+i)
 
