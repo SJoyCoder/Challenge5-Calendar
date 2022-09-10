b4bbcd8c8f8da7cec4ba1bcc0c63f69e19dbb963
Savannah McGinnis <sjmhorsegirl@verizon.net>
2022-09-10 03:45:00 -0500

    added comments and cleaned up

--- assets/script.js
+++ assets/script.js
@@ -1,37 +1,35 @@
-let time = moment().format("H");
+// declaring my variables
+// let time = 14
+let time = moment().format("HH");
 let saveBtns = document.querySelectorAll(".saveBtn");
 let appointment= document.querySelectorAll(".time-block")
 let past = document.querySelector(".past")
 
+// function to for time to update automatically and in the format I want
 function displayTime() {
-    var rightNow = moment().format('H');
+    time;
     setInterval(1000);
-    
-
 };
-console.log(time)
+console.log(time);
+// for loop to streamline my color changes and save buttons
 
 for (let i = 0; i < saveBtns.length; i++) {
-
-    if (appointment[i].getAttribute("data-time") === time){
+    
+ 
+    if (appointment[i].getAttribute("data-time") == time){
         $(appointment[i]).addClass("present");
-    }else if(appointment[i].getAttribute("data-time") < time){
-        $(appointment[i]).addClass("past");
     }else if(appointment[i].getAttribute("data-time") > time){
         $(appointment[i]).addClass("future");
-    };
-
-    $("button").click(function(){
-        $(appointment[i]).addClass("future");
-      });
-
+    }else if(appointment[i].getAttribute("data-time") < time){
+        $(appointment[i]).addClass("past");
+     };
+    
     appointment[i].value = localStorage.getItem("appt"+i)
-
-    saveBtns[i].addEventListener("click", (tacocat) =>{
-        console.log(tacocat.target);
+    
+    saveBtns[i].addEventListener("click", (save) =>{
+        console.log(save.target);
         console.log(appointment[i].value);
         localStorage.setItem("appt"+i, appointment[i].value)
-    })
-
-
+    }); 
 }
+
