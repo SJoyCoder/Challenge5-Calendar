feca36169c3a3b5b26fac77049ffb5eb402654ef
Savannah McGinnis <sjmhorsegirl@verizon.net>
2022-09-10 15:22:52 -0500

    cleaned up, fixed time, and added GIF to README

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Work Day Calendar
 
-[Work Day Calendar](https://sjoycoder.github.io/Challenge5-Calendar/)
+
+![Work Day Calendar GIF](./assets/Work%20Day%20Scheduler.gif)
+
+[Click Here To Use Work Day Calendar](https://sjoycoder.github.io/Challenge5-Calendar/)
 
 ## About
 This is a daily planner for a work day. Throughout the day the color will change showing the passage of time. Any appointments or meetings can be input and then saved in local storage with the press of a button.  
--- assets/script.js
+++ assets/script.js
@@ -1,38 +1,37 @@
 // declaring my variables
-let time = 14;
-// let time = moment().format("HH");
-let dateInput = document.querySelector("#currentDay");
+// let time = 14;
+let time = moment().format("HH");
 let saveBtns = document.querySelectorAll(".saveBtn");
 let appointment= document.querySelectorAll(".time-block")
 let past = document.querySelector(".past")
-let currentDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+
 
 
 // function to for time to update automatically and in the format I want
-function displayTime() {
-    dateInput.textContent = currentDate;
-    setInterval(1000);
-};
-displayTime();
-console.log(time);
-// for loop to streamline my color changes and save buttons
-
-for (let i = 0; i < saveBtns.length; i++) {
+function setTime() {
+    var timerInterval = setInterval(function() {
+      document.getElementById("currentDay").textContent = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+    }, 1000);
+  }
+  
+  // for loop to streamline my color changes and save buttons
+  for (let i = 0; i < saveBtns.length; i++) {
+      
+      if (appointment[i].getAttribute("data-time") == time){
+          $(appointment[i]).addClass("present");
+        }else if(appointment[i].getAttribute("data-time") > time){
+            $(appointment[i]).addClass("future");
+        }else if(appointment[i].getAttribute("data-time") < time){
+            $(appointment[i]).addClass("past");
+        };
+        
+        appointment[i].value = localStorage.getItem("appt"+i)
+        
+        saveBtns[i].addEventListener("click", (save) =>{
+            console.log(save.target);
+            console.log(appointment[i].value);
+            localStorage.setItem("appt"+i, appointment[i].value)
+        }); 
+    }
     
-    if (appointment[i].getAttribute("data-time") == time){
-        $(appointment[i]).addClass("present");
-    }else if(appointment[i].getAttribute("data-time") > time){
-        $(appointment[i]).addClass("future");
-    }else if(appointment[i].getAttribute("data-time") < time){
-        $(appointment[i]).addClass("past");
-     };
-    
-    appointment[i].value = localStorage.getItem("appt"+i)
-    
-    saveBtns[i].addEventListener("click", (save) =>{
-        console.log(save.target);
-        console.log(appointment[i].value);
-        localStorage.setItem("appt"+i, appointment[i].value)
-    }); 
-}
-
+    setTime();
